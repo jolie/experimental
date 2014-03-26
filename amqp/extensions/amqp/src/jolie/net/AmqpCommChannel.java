b994a7b5a4f39c11c27e0c23d128facb7058cba9
Michael Søby Andersen <msoa@itu.dk>
2014-03-26 08:15:05 +0000

    Removed unused imports

--- amqp/extensions/amqp/src/jolie/net/AmqpCommChannel.java
+++ amqp/extensions/amqp/src/jolie/net/AmqpCommChannel.java
@@ -6,12 +6,9 @@
 
 package jolie.net;
 
-import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
